d49472ce299359052967f775ea48241649876c3e
于馨轲 <yuxinke.to@foxmail.com>
2019-07-30 23:58:28 +0800

    连接远程仓库

---
+++ demo/src/main/java/com/example/demo/controller/DeomController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+@Controller
+public class DeomController {
+
+    
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        return "Hello World!!!";
+
+    }
+
+}
